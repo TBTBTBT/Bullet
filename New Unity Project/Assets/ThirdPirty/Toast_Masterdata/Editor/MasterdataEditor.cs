2cf4689d59ed1a50b6c24481823e2788745fe58f
田畑　翔伍 <shogo.tabata@sega.com>
2019-07-29 19:31:38 +0900

    2

--- New Unity Project/Assets/ThirdPirty/Toast_Masterdata/Editor/MasterdataEditor.cs
+++ New Unity Project/Assets/ThirdPirty/Toast_Masterdata/Editor/MasterdataEditor.cs
@@ -3,94 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
-public class MasterdataEditor : EditorWindow
+
+namespace Toast.Masterdata.Editor
 {
-    class TableData
+
+    public class MasterdataEditor : EditorWindow
     {
-        public Dictionary<string, List<string>> Data = new Dictionary<string, List<string>>();
-        string labelCache = "";
-        public void AddData(string label)
+        //classes
+
+
+        //enum
+        public enum State
         {
-            Data.Add( label,new List<string>() );
-        }
-        public void View()
-        {
-            
-            GUILayout.BeginVertical();
-
-            GUILayout.BeginHorizontal();
-          
-            labelCache = GUILayout.TextArea(labelCache);
-            
-            if (GUILayout.Button("+"))
-            {
-                labelCache = "";
-            }
-            GUILayout.EndHorizontal();
-            if (GUILayout.Button("+"))
-            {
-
-            }
-            GUILayout.EndVertical();
-        }
-    }
-    class TableToClass
-    {
-
-    }
-    class TableToJson
-    {
-
-    }
-
-    public enum State
-    {
-        Init,
-        View,
-    }
-
-    private Dictionary<State, Action<MasterdataEditor>> StateProcess = new Dictionary<State, Action<MasterdataEditor>>()
-    {
-        {
-            State.Init,
-            self =>
-            {
-                self.Table = new TableData();
-                self.Next(State.View);
-              
-            }
-        },
-        {
-            State.View,
-            self =>
-            {
-                self.Table?.View();   
-            }
+            Init,
+            View,
         }
 
-    };
-    State Current { get; set; } = State.Init;
-    TableData Table { get; set; }
-    [MenuItem("Toast/MasterdataEditor")]
-    static void Open()
-    {
-        var window = CreateInstance<MasterdataEditor>();
-        window.Show();
-    }
-    void UIDraw()
-    {
+
+        //process
+
+        private Dictionary<State, Action<MasterdataEditor>> StateProcess =
+            new Dictionary<State, Action<MasterdataEditor>>()
+            {
+                {
+                    State.Init,
+                    self =>
+                    {
+                        self.Table = new TableData();
+                        self.Next(State.View);
+
+                    }
+                },
+                {
+                    State.View,
+                    self => { self.Table?.View(); }
+                }
+
+            };
+
+        State Current { get; set; } = State.Init;
+        private State NextState { get; set; } = State.Init;
+        TableData Table { get; set; }
+
+        [MenuItem("Toast/MasterdataEditor")]
+        static void Open()
+        {
+            var window = CreateInstance<MasterdataEditor>();
+            window.Show();
+        }
+
+        void UIDraw()
+        {
+
+        }
+
+        void Next(State state)
+        {
+            NextState = state;
+        }
+
+        private void Update()
+        {
+            if (Current != NextState)
+            {
+                Current = NextState;
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (StateProcess.ContainsKey(Current))
+            {
+                StateProcess[Current]?.Invoke(this);
+            }
+        }
 
     }
-    void Next(State state)
-    {
-        Current = state;
-    }
-    private void OnGUI()
-    {
-        if (StateProcess.ContainsKey(Current))
-        {
-            StateProcess[Current]?.Invoke(this);
-        }
-    }
-    
+
 }
---
+++ New Unity Project/Assets/ThirdPirty/Toast_Masterdata/Editor/MasterdataEditorClasses.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Toast.Masterdata.Editor
+{
+
+
+    public class TableData
+    {
+        public Dictionary<string, List<string>> Data = new Dictionary<string, List<string>>();
+        string labelCache = "";
+        private int selectedIndexCache = 0;
+        public void AddData(string label)
+        {
+            if (label != "" && !Data.ContainsKey(label))
+            {
+                Data.Add(label, new List<string>());
+            }
+
+            CheckDataLength();
+        }
+
+        void RemoveData(string label)
+        {
+            if (label != "" && Data.ContainsKey(label))
+            {
+                Data.Remove(label);
+            }
+        }
+
+        void AddRow()
+        {
+            foreach (var list in Data)
+            {
+                list.Value.Add("");
+            }
+
+            CheckDataLength();
+        }
+        void RemoveRow(int row)
+        {
+            foreach (var list in Data)
+            {
+                list.Value.RemoveAt(row);
+            }
+
+        }
+        public void View()
+        {
+
+            GUILayout.BeginVertical(GUI.skin.box);
+            //ラベル表示
+            AddColumnButton();
+            
+            GUILayout.BeginHorizontal();
+            ViewLabel(Data.Keys.ToArray()); 
+
+            GUILayout.EndHorizontal();
+            ViewData(Data.Values.ToArray());
+            //カラム表示
+            GUILayout.BeginHorizontal();
+            GUILayout.EndHorizontal();
+            AddRowButton();
+            GUILayout.EndVertical();
+        }
+
+
+        private void ViewLabel(string[] texts)
+        {
+            if (texts.Length <= 0)
+            {
+                return;
+                selectedIndexCache =
+                    GUILayout.SelectionGrid(selectedIndexCache, texts, texts.Length, EditorStyles.miniButtonMid);
+            }
+
+            foreach (var text in texts)
+            {
+                GUILayout.Label(text, EditorStyles.miniButtonMid);
+
+                //公式によるとリペイント時に限るらしいがこれでうまくいってるので…
+                var rect = GUILayoutUtility.GetLastRect();
+
+                LabelRightClickEvent(text,rect);
+                
+            }
+        }
+        private void LabelRightClickEvent(string label, Rect rect)
+        {
+            var ev = Event.current;
+            if (ev.type != EventType.MouseDown || ev.button != 1 || !rect.Contains(ev.mousePosition))
+            {
+                return;
+            }
+
+            var menu = new GenericMenu();
+            menu.AddItem(new GUIContent("Delete " + label), false, () => { RemoveData(label); });
+            menu.ShowAsContext();
+            ev.Use();
+
+
+        }
+
+        private void CheckDataLength()
+        {
+            var max = 0;
+            foreach (var list in Data)
+            {
+                max = Math.Max(list.Value.Count ,max) ;
+            }
+
+            foreach (var list in Data)
+            {
+                var d = max - list.Value.Count;
+                if (d > 0)
+                {
+                    var l = new string[d];
+                    list.Value.AddRange(l);
+                }
+            }
+        }
+
+        private void ViewData(List<string>[] data)
+        {
+            GUILayout.BeginHorizontal();
+            foreach (var list in data)
+            {
+                GUILayout.BeginVertical(GUI.skin.box);
+                for (var i = 0; i < list.Count; i++)
+                {
+                    list[i] = GUILayout.TextArea(list[i]);
+                }
+                GUILayout.EndVertical();
+            }
+            GUILayout.EndHorizontal();
+        }
+        //足りないところを埋める
+        private void FillData()
+        {
+
+        }
+        //列追加
+        private void AddColumnButton()
+        {
+            labelCache = GUILayout.TextArea(labelCache);
+            if (GUILayout.Button("+"))
+            {
+                AddData(labelCache);
+                labelCache = "";
+            }
+
+        }
+        //要素追加
+        private void AddRowButton()
+        {
+            if (GUILayout.Button("+"))
+            {
+                AddRow();
+            }
+
+        }
+    }
+
+    class TableToClass
+    {
+
+    }
+
+    class TableToJson
+    {
+
+    }
+
+}
