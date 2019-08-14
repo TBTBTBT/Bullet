2aa87e3f163ed954ead74535b15c44eedfe190d1
田畑　翔伍 <s.toaster.app@gmail.com>
2019-08-14 16:57:56 +0900

    11

---
+++ New Unity Project/Assets/Project/Scripts/Map/MapManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public enum PlaceType
+    {
+        None = 0,
+        Normal,
+        Shop,
+        Start,
+        Village,
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class Map
+{
+    [SerializeField]
+    private int[,] data;
+
+    public int[,] Data => data;
+
+
+}
