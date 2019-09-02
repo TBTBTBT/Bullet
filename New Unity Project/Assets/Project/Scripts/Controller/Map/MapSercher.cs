869867505fd37a4ce5a4b363fd2bad7b15455d5e
Toasterapp <s.toaster.app@gmail.com>
2019-09-03 00:21:59 +0900

    33

--- New Unity Project/Assets/Project/Scripts/Controller/Map/MapSercher.cs
+++ New Unity Project/Assets/Project/Scripts/Controller/Map/MapSercher.cs
@@ -21,47 +21,91 @@ public class MapSercher
         Result.Clear();
         var count = 0;
         var beforeLength = 0;
-        foreach (var reach in CanReach(map,pos,num))
+        //foreach (var reach in CanReach(map,pos,num))
+        //{
+
+        //    if (Result.Any(r => r == reach))
+        //    {
+        //        continue;
+        //    }
+        //    Result.Add(reach);
+        //    count++;
+        //    if (count > 1000)
+        //    {
+        //        if (beforeLength != Result.Count)
+        //        {
+        //            OnUpdateResult?.Invoke();
+        //        }
+        //        beforeLength = Result.Count;
+        //        yield return null;
+        //        count = 0;
+        //    }
+        //    yield return null;
+        //}
+        var list = new List<(int before, List<int> list)>() { (-1,new List<int>() { pos}) };
+        while(num > 0)
         {
-
-            if (Result.Any(r => r == reach))
-            {
-                continue;
-            }
-            Result.Add(reach);
-            count++;
-            if (count > 1000)
-            {
-                if (beforeLength != Result.Count)
+            Debug.Log(num);
+            var nextList = new List<(int before, List<int> list)>();
+            foreach (var l in list) {
+                Debug.Log("l : " +l.before);
+                foreach (var l2 in l.list)
                 {
-                    OnUpdateResult?.Invoke();
+                    nextList.Add(CanReach(map, l2, num, l.before));
+                    Debug.Log("l2 : " + nextList.Count);
+                    yield return null;
                 }
-                beforeLength = Result.Count;
                 yield return null;
-                count = 0;
-            }
-        }
 
+            }
+            list.Clear();
+            list = nextList;
+            num = num - 1;
+            yield return null;
+        }
+        foreach(var l in list)
+        {
+            Result.Add(l.before);
+        }
+        
         yield return null;
     }
-
-    private IEnumerable<int> CanReach(StationModel[] map, int start,int length,int before = -1)
+    private (int before, List<int> list) CanReach(StationModel[] map, int start, int length, int before = -1)
     {
         if (length == 0)
         {
-            yield return start;
+            return (start,new List<int>() { start });
         }
+        Debug.Log(length);
+        var list = new List<int>();
         foreach (var r in map[start].Relation)
         {
             if (r != before)
             {
-                foreach (var reach in CanReach(map, r, length - 1, start))
-                {
-                    yield return reach;
-                }
+                list.Add(r);
             }
 
         }
+        return (start,list);
     }
+    //private IEnumerable<int> CanReach(StationModel[] map, int start,int length,int before = -1)
+    //{
+    //    if (length == 0)
+    //    {
+    //        yield return start;
+    //    }
+    //    Debug.Log(length);
+    //    foreach (var r in map[start].Relation)
+    //    {
+    //        if (r != before)
+    //        {
+    //            foreach (var reach in CanReach(map, r, length - 1, start))
+    //            {
+    //                yield return reach;
+    //            }
+    //        }
+
+    //    }
+    //}
 
 }
