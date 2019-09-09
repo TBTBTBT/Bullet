659881b6c2af05f05b9766101a238e9708042fe6
Toasterapp <s.toaster.app@gmail.com>
2019-09-10 01:13:59 +0900

    40

---
+++ UISample/New Unity Project/Assets/Scripts/UI/UIElement/IUIElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum UIState
+{
+    Loading,
+    Stable
+}
+public interface IUIState
+{
+    UIState State { get; set; }
+    void StartLoading();
+    void OnLoaded();
+}
